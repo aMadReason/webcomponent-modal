5d12d9dfeb515c838d7f40f5861271267ce1b5ee
aMadReason <aMadReason@users.noreply.github.com>
2020-01-20 12:26:37 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
-  <div id="app">
-    <div class="listing">
-      <article v-bind:key="i" v-for="(v, i) in entries">
-        <h2>{{v.title}}</h2>
-        <img v-bind:src="v.img" alt>
-        <button @click="e => openModal(e, i)">Info</button>
-      </article>
+  <div>
+    <article class="note">
+      <h1>Simple Modal Web Component</h1>
+      <p>
+        Based on wai-aria gudelines:
+        <a
+          href="https://www.w3.org/TR/wai-aria-practices/#dialog_modal"
+        >Dialog Modal</a>.
+      </p>
+
+      <strong>Features</strong>
+      <ul>
+        <li>Keyboard / Screen Reader (NVDA) accessible</li>
+        <li>Click-On-Overlay or Escape key will close without close button.</li>
+        <li>Mobile First / Responsive</li>
+        <li>Tested in latest FF, Chrome, IE Edge</li>
+      </ul>
+
+      <hr>
+
+      <strong>Requires:</strong>
+
+      <p>The webcomponent is intended for use in an es6+ setup with transpilation.</p>
+      <p>Web component polyfills, use the below for rapid prototyping, but remember to include these scripts in you're source.</p>
+      <p>DO NOT transpile the polyfills, the world will break.</p>
+
+      <pre>
+        &lt;script src="https://cdnjs.com/libraries/webcomponents.js"&gt; &lt;/script&gt;
+        &lt;script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.4.1/custom-elements-es5-adapter.js"&gt; &lt;/script&gt;
+      </pre>
+
+      <hr>
+      <strong>Syntax</strong>
+      <p>Import the file in js</p>
+      <pre>
+        import '{path to component}/wc-modal-vanilla.js';
+      </pre>
+
+      <p>(or) Import via module script in html (not tested yet!!).</p>
+      <pre>
+        &lt;script type="module" src="{path to component}/wc-modal-vanilla.js"&gt; &lt;/script&gt;
+      </pre>
+
+      <p>The use tag in html (basic usage) then use a button with an event to change 'data-open' to 'true' to open it.</p>
+
+      <pre>
+        &lt;wc-modal data-open="false"&gt;
+          I am the modal content
+        &lt;/wc-modal&gt;
+
+        &lt;button id="modaltrigger"&gt;
+          open modal
+        &lt;/button&gt;
+
+        &lt;script&gt;
+        
+        const modal = document.getElementById('wc-modal');
+        const modaltrigger = document.getElementById('modaltrigger');
+
+        modaltrigger.addEcentListener(e, () => {
+          modal.open({ trigger: modaltrigger}); // most accessible as focus will return to trigger on close.
+
+          // modal.setAttribute('data-open', true); // this will also work
+        })
+        
+        &lt;/script&gt;
+      </pre>
+
+      <p>For advanced usage see the source in this demo where the modal is used inside of a vue instance.</p>
+    </article>
+
+    <div id="app">
+      <div class="listing">
+        <article v-bind:key="i" v-for="(v, i) in entries">
+          <h2>{{v.title}}</h2>
+          <img v-bind:src="v.img" alt>
+          <button @click="e => openModal(e, i)">Info</button>
+        </article>
+      </div>
+      <wc-modal ref="modal" v-bind:data-open="isOpen" v-on:wc-event="updateModal">
+        <h1>{{entry && entry.title}}</h1>
+        <a href="#">Test</a>
+        <p>{{ entry && entry.content}}</p>
+      </wc-modal>
     </div>
-    <wc-modal ref="modal" v-bind:data-open="isOpen" v-on:wc-event="updateModal">
-      <h1>{{entry && entry.title}}</h1>
-      <a href="#">Test</a>
-      <p>{{ entry && entry.content}}</p>
-    </wc-modal>
   </div>
 </template>
 
@@ -58,7 +130,6 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
